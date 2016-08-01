a5ca99b1f68c2d6adaf0f4a9013bc714c3bbd778
tesscorinne <tesscorinnemiller@gmail.com>
2016-08-01 11:45:53 -0400

    Update mfmformsubmission.js

--- javascripts/mfmformsubmission.js
+++ javascripts/mfmformsubmission.js
@@ -17,6 +17,7 @@ jQuery( document ).ready(function( $ ) {
 		// Note: we disable elements AFTER the form data has been serialized.
 		// Disabled form elements will not be serialized.
 		$inputs.prop("disabled", true);
+		var $validate = $('#foo').validate();
 		$('#result').text('Sending data...');
 		// fire off the request to /form.php
 		request = $.ajax({
